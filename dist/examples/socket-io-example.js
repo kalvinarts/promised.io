ed0e5f6724e3ab06e71506f49906474d42872aa6
kalvin <kalvin@unseen.is>
2017-01-16 03:38:20 +0100

    browser

--- dist/examples/socket-io-example.js
+++ dist/examples/socket-io-example.js
@@ -15,26 +15,26 @@ var _socket4 = _interopRequireDefault(_socket3);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Setup a socket.io server
-const ioPort = 3131;
-const io = (0, _socket2.default)().listen(ioPort);
+var ioPort = 3131;
+var io = (0, _socket2.default)().listen(ioPort);
 
 // Define the API that the server will expose
-const apiFuncs = {
-  square: num => {
+var apiFuncs = {
+  square: function square(num) {
     return num * num;
   },
-  sqrt: num => {
+  sqrt: function sqrt(num) {
     return Math.sqrt(num);
   }
 };
 
 // First we wait for connections to the server
-io.on('connection', socket => {
+io.on('connection', function (socket) {
   console.log('-- server - connection recieved');
 
   // When a new connection is recieved we wrap
   // the socket into a PromisedIO instance
-  const promisedAPI = new _index2.default(
+  var promisedAPI = new _index2.default(
   // First argument is the event emitter we want to wrap 
   socket,
   // Optionally we can pass the API object as a second argument
@@ -49,24 +49,24 @@ io.on('connection', socket => {
   // Another way to describe our API is to use the .on function on the
   // PromisedIO instance the same way we do to a normal socket from
   // socket.io
-  promisedAPI.on('sayHelloTo', name => {
-    return `Hello ${ name }!`;
-  }).on('testContext', (arg, ctx) => {
+  promisedAPI.on('sayHelloTo', function (name) {
+    return 'Hello ' + name + '!';
+  }).on('testContext', function (arg, ctx) {
     return ctx;
   })
   // The .on method returns the PromisedIO instance
   // so we can chain the calls.
-  .on('asyncWait', secs => {
+  .on('asyncWait', function (secs) {
     // Our API functions can return promise, and the client
     // will recieve the result when the promise is fulfilled
     // or rejected.
-    return new Promise((f, r) => {
+    return new Promise(function (f, r) {
       // As a simple example we simply fulfill the promise after
       // a given number of seconds
       console.log('-- server: waiting', secs, 'seconds');
-      setTimeout(() => {
+      setTimeout(function () {
         console.log('-- server: waited', secs, 'seconds');
-        f(`Waited for ${ secs } to return this string.`);
+        f('Waited for ' + secs + ' to return this string.');
       }, secs * 1000);
     });
   });
@@ -75,45 +75,47 @@ io.on('connection', socket => {
 // Now that we have our API defined we can connect to it.
 
 // Setup a socket.io-client
-const socket = new _socket4.default(`ws://localhost:${ ioPort }`);
+var socket = new _socket4.default('ws://localhost:' + ioPort);
 
 // Now we wrap the socket the same way we did to the server socket
-const promisedClient = new _index2.default(socket);
+var promisedClient = new _index2.default(socket);
 
 // An we are ready to go! To call methods on the server
 // just pass the function name and the argument object to
 // .emit method of the PromiseEmitter instance
-promisedClient.emit('square', 5).then(result => {
-  console.log(`-- client: The sqare method returned ${ result }`);
+promisedClient.emit('square', 5).then(function (result) {
+  console.log('-- client: The sqare method returned ' + result);
 });
 
 // There is a builtin helper to retrieve the registered methods
 // on the other side. We can use it to create a real API objcet 
 // of functions to call.
-promisedClient.emit('_getAPIMethods').then(methods => {
-  let clientAPI = {};
+promisedClient.emit('_getAPIMethods').then(function (methods) {
+  var clientAPI = {};
 
   console.log('-- client: The server exposes the methods:', methods);
 
-  methods.forEach(method => {
+  methods.forEach(function (method) {
     console.log('  -', method);
-    clientAPI[method] = argument => promisedClient.emit(method, argument);
+    clientAPI[method] = function (argument) {
+      return promisedClient.emit(method, argument);
+    };
   });
 
   // And now just use it like a regular API which methods return
   // a Promise ;)
-  clientAPI.testContext().then(result => {
+  clientAPI.testContext().then(function (result) {
     console.log('-- client: server context =>', result);
   });
 
-  clientAPI.asyncWait(5).then(result => {
+  clientAPI.asyncWait(5).then(function (result) {
     console.log('-- client: asyncWait method says:', result);
     process.exit(0);
-  }).catch(err => {
+  }).catch(function (err) {
     console.log('call failed:', err);
     process.exit(1);
   });
-}).catch(err => {
+}).catch(function (err) {
   console.log('Something bad happenned...');
   throw err;
 });
--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,38 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 // Generates a random unique identifier (12B hex by default)
-const genUID = (rBytes = 8, map = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e']) => {
-  let now = new Date().getTime();
-  let nowS = ((now - now % 1000) / 1000).toString(16); // 4B timeStamp
-  let randA = [];
-  for (let c = 0; c < rBytes * 2; c++) randA.push(map[Math.floor(Math.random() * map.length)]);
-  return nowS + randA.join('');
+var genUID = function genUID() {
+  var rBytes = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 8;
+  var map = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e'];
+
+  var now = new Date().getTime();
+  var nowS = ((now - now % 1000) / 1000).toString(16); // 4B timeStamp
+  var randA = [];
+  for (var c = 0; c < rBytes * 2; c++) {
+    randA.push(map[Math.floor(Math.random() * map.length)]);
+  }return nowS + randA.join('');
 };
 
-class PromisedIO {
-  constructor(emitter, api = {}, debug = false, context = null, uid = '__pEmitter') {
+var PromisedIO = function () {
+  function PromisedIO(emitter) {
+    var api = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    var debug = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+    var _this = this;
+
+    var context = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
+    var uid = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : '__pEmitter';
+
+    _classCallCheck(this, PromisedIO);
+
     this._emitter = emitter;
     this._debug = debug;
     this._ctx = context;
@@ -25,127 +46,156 @@ class PromisedIO {
     if (api) this.registerAPI(api);
     // Add a helper for the client to retrieve the
     // available methods list
-    this.on('_getAPIMethods', () => {
-      return this._apiMethods;
+    this.on('_getAPIMethods', function () {
+      return _this._apiMethods;
     });
   }
 
   // Check if a response listener is registered
-  _listenerIsRegistered(event) {
-    for (i in this._events) {
-      if (this._events[i] === event) return true;
+
+
+  _createClass(PromisedIO, [{
+    key: '_listenerIsRegistered',
+    value: function _listenerIsRegistered(event) {
+      for (i in this._events) {
+        if (this._events[i] === event) return true;
+      }
+      return false;
     }
-    return false;
-  }
 
-  // Registers an event to handle its responses
-  _registerListener(event) {
-    this._emitter.on(this._uid + '_R_' + event, res => {
-      this._unresolved = this._unresolved.filter(call => {
-        if (call.uid === res.uid) {
-          if (res.error) {
-            call.r(res.error);
-          } else {
-            call.f(res.data);
-          }
-          return false;
-        } else {
-          return true;
-        }
-      });
-    });
-  }
+    // Registers an event to handle its responses
 
-  // Emits an event and returns a promise to handle the response
-  emit(event, data) {
-    // If it's not already done, register a handler for the responses of the event
-    if (!this._listenerIsRegistered(event)) this._registerListener(event);
-    // Return a promise
-    return new Promise((f, r) => {
-      // Generate a unique ID for this call
-      let uid = genUID();
-      // Store the unresolved promise with its UID to keep track on it
-      this._unresolved.push({
-        uid: uid,
-        f: f,
-        r: r
-      });
-      // Emit the event with the same UID we stored for the promise
-      this._emitter.emit(this._uid + '_C_' + event, {
-        uid: uid,
-        data: data
-      });
-    });
-  }
+  }, {
+    key: '_registerListener',
+    value: function _registerListener(event) {
+      var _this2 = this;
 
-  // Registers an event listener
-  on(event, handler) {
-    // Register an event listener
-    this._emitter.on(this._uid + '_C_' + event, e => {
-      // Helper for resolving handlers that return a Promise
-      const resolvePromisedResult = result => {
-        if (result instanceof Promise || typeof result === 'object' && typeof result.then === 'function') {
-          // Wait for the Promise to be fulfilled or rejected
-          let p = result.then(res => {
-            resolvePromisedResult(res);
-          }).catch(err => {
-            // If debugging is enabled send errors to the other end
-            if (this._debug) {
-              p.catch(err => {
-                this._emitter.emit(this._uid + '_R_' + event, {
-                  uid: e.uid,
-                  error: err
-                });
-              });
+      this._emitter.on(this._uid + '_R_' + event, function (res) {
+        _this2._unresolved = _this2._unresolved.filter(function (call) {
+          if (call.uid === res.uid) {
+            if (res.error) {
+              call.r(res.error);
             } else {
-              throw err;
+              call.f(res.data);
             }
-          });
-        } else {
-          // Emit the response back
-          this._emitter.emit(this._uid + '_R_' + event, {
-            uid: e.uid,
-            data: result
-          });
-        }
-      };
-      // If debugging is enabled send errors to the other end
-      if (this._debug) {
-        try {
-          // Call the handler and resolve the return if it's a Promise
-          resolvePromisedResult(handler(e.data, this._ctx));
-        } catch (err) {
-          this._emitter.emit(this._uid + '_R_' + event, {
-            uid: e.uid,
-            error: err
-          });
-        }
-      } else {
-        // Call the handler and resolve the return if it's a Promise without handling any errors   
-        resolvePromisedResult(handler(e.data, this._ctx));
-      }
-    });
-    // Store the method name to this._apiMethods
-    let found = false;
-    this._apiMethods.forEach(method => {
-      if (method === event) found = true;
-    });
-    if (!found) this._apiMethods.push(event);
-    // Let chain calls
-    return this;
-  }
+            return false;
+          } else {
+            return true;
+          }
+        });
+      });
+    }
 
-  // Registers a set of functions in batch
-  registerAPI(obj) {
-    let funcNames = Object.keys(obj);
-    funcNames.forEach(name => {
-      if (typeof obj[name] === 'function') {
-        this.on(name, obj[name]);
-      }
-    });
-    return this;
-  }
-}
+    // Emits an event and returns a promise to handle the response
+
+  }, {
+    key: 'emit',
+    value: function emit(event, data) {
+      var _this3 = this;
+
+      // If it's not already done, register a handler for the responses of the event
+      if (!this._listenerIsRegistered(event)) this._registerListener(event);
+      // Return a promise
+      return new Promise(function (f, r) {
+        // Generate a unique ID for this call
+        var uid = genUID();
+        // Store the unresolved promise with its UID to keep track on it
+        _this3._unresolved.push({
+          uid: uid,
+          f: f,
+          r: r
+        });
+        // Emit the event with the same UID we stored for the promise
+        _this3._emitter.emit(_this3._uid + '_C_' + event, {
+          uid: uid,
+          data: data
+        });
+      });
+    }
+
+    // Registers an event listener
+
+  }, {
+    key: 'on',
+    value: function on(event, handler) {
+      var _this4 = this;
+
+      // Register an event listener
+      this._emitter.on(this._uid + '_C_' + event, function (e) {
+        // Helper for resolving handlers that return a Promise
+        var resolvePromisedResult = function resolvePromisedResult(result) {
+          if (result instanceof Promise || (typeof result === 'undefined' ? 'undefined' : _typeof(result)) === 'object' && typeof result.then === 'function') {
+            (function () {
+              // Wait for the Promise to be fulfilled or rejected
+              var p = result.then(function (res) {
+                resolvePromisedResult(res);
+              }).catch(function (err) {
+                // If debugging is enabled send errors to the other end
+                if (_this4._debug) {
+                  p.catch(function (err) {
+                    _this4._emitter.emit(_this4._uid + '_R_' + event, {
+                      uid: e.uid,
+                      error: err
+                    });
+                  });
+                } else {
+                  throw err;
+                }
+              });
+            })();
+          } else {
+            // Emit the response back
+            _this4._emitter.emit(_this4._uid + '_R_' + event, {
+              uid: e.uid,
+              data: result
+            });
+          }
+        };
+        // If debugging is enabled send errors to the other end
+        if (_this4._debug) {
+          try {
+            // Call the handler and resolve the return if it's a Promise
+            resolvePromisedResult(handler(e.data, _this4._ctx));
+          } catch (err) {
+            _this4._emitter.emit(_this4._uid + '_R_' + event, {
+              uid: e.uid,
+              error: err
+            });
+          }
+        } else {
+          // Call the handler and resolve the return if it's a Promise without handling any errors   
+          resolvePromisedResult(handler(e.data, _this4._ctx));
+        }
+      });
+      // Store the method name to this._apiMethods
+      var found = false;
+      this._apiMethods.forEach(function (method) {
+        if (method === event) found = true;
+      });
+      if (!found) this._apiMethods.push(event);
+      // Let chain calls
+      return this;
+    }
+
+    // Registers a set of functions in batch
+
+  }, {
+    key: 'registerAPI',
+    value: function registerAPI(obj) {
+      var _this5 = this;
+
+      var funcNames = Object.keys(obj);
+      funcNames.forEach(function (name) {
+        if (typeof obj[name] === 'function') {
+          _this5.on(name, obj[name]);
+        }
+      });
+      return this;
+    }
+  }]);
+
+  return PromisedIO;
+}();
 
 exports.default = PromisedIO;
 //# sourceMappingURL=index.js.map
